6477fb79382994912c84507d341056a6c033e878
Piyush289kumar <piyushraikwar289@gmail.com>
2026-01-13 23:53:25 +0530

    Create Beto Grid.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,25 @@
 "use client";
 import { PartnerBrandMarquee } from "@/components/magic-ui/PartnerBrandMarquee";
 import HeroSection from "./components/ui/HeroSection";
+import PrimaryHeading from "@/components/ui/heading/PrimaryHeading";
+import { BentoGridThird } from "@/components/aceternity-ui/BentoGrid";
 
 export default function Home() {
   return (
     <>
-    <div className="bg-black">
-      <HeroSection />
-      <PartnerBrandMarquee />
-    </div>
+      <div className="bg-black">
+        <HeroSection />
+        <PartnerBrandMarquee />
+        {/* Bento Grid Start */}
+        <div className="py-36">
+          <PrimaryHeading
+            heading="Empower Your Workflow with AI"
+            des="Ask your AI Agent for real-time collaboration, seamless integrations, and actionable insghts to streamline your operations."
+          />
+          <BentoGridThird />
+        </div>
+        {/* Bento Grid End */}
+      </div>
     </>
   );
 }
