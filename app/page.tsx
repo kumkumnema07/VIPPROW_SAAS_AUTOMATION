339d7177dd313e7809ee50bf1c9f1cbedaf9fcf9
Piyush289kumar <piyushraikwar289@gmail.com>
2026-01-13 23:23:29 +0530

    Brand Marquee.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 "use client";
+import { PartnerBrandMarquee } from "@/components/magic-ui/PartnerBrandMarquee";
 import HeroSection from "./components/ui/HeroSection";
 
 export default function Home() {
   return (
     <>
+    <div className="bg-black">
       <HeroSection />
+      <PartnerBrandMarquee />
+    </div>
     </>
   );
 }
---
+++ components/magic-ui/PartnerBrandMarquee.tsx
@@ -0,0 +1,94 @@
+import { cn } from "@/lib/utils";
+import { Marquee } from "../ui/marquee";
+import Image from "next/image";
+
+const reviews = [
+  {
+    name: "Jack",
+    username: "@jack",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://avatar.vercel.sh/jack",
+  },
+  {
+    name: "Jill",
+    username: "@jill",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://avatar.vercel.sh/jill",
+  },
+  {
+    name: "John",
+    username: "@john",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://avatar.vercel.sh/john",
+  },
+  {
+    name: "Jane",
+    username: "@jane",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://avatar.vercel.sh/jane",
+  },
+  {
+    name: "Jenny",
+    username: "@jenny",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://avatar.vercel.sh/jenny",
+  },
+  {
+    name: "James",
+    username: "@james",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://avatar.vercel.sh/james",
+  },
+];
+
+const firstRow = reviews.slice(0, reviews.length / 2);
+const secondRow = reviews.slice(reviews.length / 2);
+
+const ReviewCard = ({
+  img,
+  name,
+  username,
+  body,
+}: {
+  img: string;
+  name: string;
+  username: string;
+  body: string;
+}) => {
+  return (
+    <figure
+      className={cn(
+        "relative h-full w-64 cursor-pointer overflow-hidden rounded-xl border p-4 bg-black",
+        // light styles
+        "border-black/[.1] bg-black/[.01] hover:bg-black/[.05]",
+        // dark styles
+        "dark:border-black/[.1] dark:bg-black/[.10] dark:hover:bg-black/[.15]"
+      )}
+    >
+      <div className="flex flex-row items-center gap-2">
+        <Image
+          src={
+            "https://framerusercontent.com/images/otv1rEDn2X7h8TFtKPCksQmAEKQ.svg?width=75&height=17"
+          }
+          alt="Brand Name"
+          height={100}
+          width={120}
+        />
+      </div>
+    </figure>
+  );
+};
+
+export function PartnerBrandMarquee() {
+  return (
+    <div className="relative flex w-full max-w-7xl flex-col items-center justify-center overflow-hidden mx-auto bg-black h-44">
+      <Marquee pauseOnHover className="[--duration:20s]">
+        {firstRow.map((review) => (
+          <ReviewCard key={review.username} {...review} />
+        ))}
+      </Marquee>
+      <div className="from-background pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-black-to-r"></div>
+      <div className="from-background pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-black-to-l"></div>
+    </div>
+  );
+}
