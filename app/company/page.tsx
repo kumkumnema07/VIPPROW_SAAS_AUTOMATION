e093e7ac594b037405be1c68621328114f66011d
Piyush289kumar <piyushraikwar289@gmail.com>
2026-01-14 11:32:21 +0530

    Create Feature Section.

--- app/company/page.tsx
+++ app/company/page.tsx
@@ -1,9 +1,11 @@
 import { BackgroundRippleEffect } from "@/components/aceternity-ui/BackgroundRippleEffec";
+import { FeaturesSection } from "@/components/aceternity-ui/sections/FeatureSection";
 
 export default function CompanyPage() {
   return (
     <>
       <BackgroundRippleEffect />
+      <FeaturesSection />
     </>
   );
 }
