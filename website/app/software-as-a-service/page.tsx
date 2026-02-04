e4012d058482024737edc4e26692b3c09663ad7f
Piyush289kumar <piyushraikwar289@gmail.com>
2026-02-04 18:04:43 +0530

    Create SaaS Feature Section.

--- website/app/software-as-a-service/page.tsx
+++ website/app/software-as-a-service/page.tsx
@@ -2,6 +2,7 @@ import ClientCTA from "@/components/client-sections/ClientCTA";
 import ClientTestimonialMarquee from "@/components/client-sections/ClientTestimonialMarquee";
 import SaasHeroSection from "@/components/custom-ui/SaasHeroSection";
 import WorkFlowSteps from "@/components/custom-ui/WorkFlowSteps";
+import SaasFeatureSteps from "@/components/mvpblock-ui/SaasFeatureSteps";
 import TopProductSparkles from "@/components/mvpblock-ui/TopProductSparkles";
 import ServiceGridScroller from "@/components/ui/cards/ServiceGridScroller";
 import PrimaryHeading from "@/components/ui/heading/PrimaryHeading";
@@ -12,15 +13,21 @@ export default function SoftwareAsAServicePage() {
       {/* Saas Hero Section Start */}
       <SaasHeroSection />
       {/* Saas Hero Section End */}
-
       {/* work flow steps start */}
       <WorkFlowSteps />
       {/* work flow steps end */}
-
       {/* Top Product Sparkles Start */}
       <TopProductSparkles />
       {/* Top Product Sparkles Start */}
-
+      {/* Testimonial Start */}
+      <div className="pt-20 max-w-7xl mx-auto">
+        <PrimaryHeading
+          heading="Empower Your Workflow with AI"
+          des="Ask your AI Agent for real-time collaboration, seamless integrations, and actionable insghts to streamline your operations."
+        />
+        <SaasFeatureSteps />
+      </div>
+      {/* Testimonial End */}
       {/* Services Start */}
       <ServiceGridScroller />
       {/* Services End */}
@@ -34,7 +41,6 @@ export default function SoftwareAsAServicePage() {
         <ClientTestimonialMarquee />
       </div>
       {/* Testimonial End */}
-
       {/* CTA Start */}
       <div className="pt-20 max-w-7xl mx-auto">
         <ClientCTA />
