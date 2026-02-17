b4ff490460b3f6d0517c20602f8066e785e43698
kumkumnema07 <kumkumnema7@gmail.com>
2026-02-17 15:46:26 +0530

    journey

--- website/components/ui/cards/Journey.tsx
+++ website/components/ui/cards/Journey.tsx
@@ -1,26 +1,26 @@
 export default function Journey() {
   return (
-    <section className=" text-white py-24 px-6">
+    <section className=" text-white py-12  px-6">
       <div className="max-w-5xl mx-auto">
         
         {/* Card */}
-        <div className="bg-red-400 rounded-3xl p-10 md:p-16 shadow-2xl  ">
+        <div className="  ">
           
 
          
 
           {/* Content */}
-          <div className="space-y-6 text-slate-300 leading-relaxed text-lg">
+          <div className="space-y-6 text-slate-300 leading-relaxed text-sm md:text-justify ">
 
             <p>
-              At <span className="text-white font-semibold">Vipprow</span>, our journey began with a simple belief — growth should be intentional, measurable, and built on strategy, not guesswork. The vision behind our journey is to transform how businesses experience digital growth by combining performance marketing, data intelligence, and smart automation into structured growth systems.
+              At <span className="text-white  text-xs md:text-lg">Vipprow</span>, our journey began with a simple belief — growth should be intentional, measurable, and built on strategy, not guesswork. The vision behind our journey is to transform how businesses experience digital growth by combining performance marketing, data intelligence, and smart automation into structured growth systems.
             </p>
 
-            <p>
+            <p className=" md:text-lg">
               We saw that many businesses were spending on marketing without clarity or predictable outcomes. That’s where Vipprow stepped in — to create a smarter way forward. Our vision is to help brands move beyond random boosting and disconnected efforts, and instead operate with clear strategies, optimized funnels, and scalable systems.
             </p>
 
-            <p>
+            <p className=" md:text-lg">
               Every step of our journey has been guided by innovation, transparency, and a results-first mindset. We aim to build digital ecosystems where businesses can scale confidently, automate efficiently, and make decisions backed by real data.
             </p>
 
